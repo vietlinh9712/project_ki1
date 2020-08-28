c27002ec56e37c885266e5227991f9a804a40896
Việt Lê <64905071+vietlinh9712@users.noreply.github.com>
2020-08-28 09:02:12 +0700

    viet

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.post('/',function (req,res,next) {
+  console.log(req.body);
+  if (req.body.input === 'cm'){
+    res.send({
+      cm: 100
+    })
+  }
+})
+
 module.exports = router;
