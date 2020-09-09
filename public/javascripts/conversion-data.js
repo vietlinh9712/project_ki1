31072dee12a053391e405baa8d76e188a04bd1a8
Việt Lê <64905071+vietlinh9712@users.noreply.github.com>
2020-09-09 17:07:01 +0700

    update conversion-data js

--- public/javascripts/conversion-data.js
+++ public/javascripts/conversion-data.js
@@ -1,39 +1,63 @@
 let conversionRateFromDatabase = [];
 let url = document.location.pathname;
 let key = url.replace('/','');
-$.ajax({
-    url: '/api/getRate',
-    method: 'post',
-    data: {
-        key: key
-    },
-    success: function (result){
-        calculationUsingRateFromServer(result);
-    }
-})
+let allInputField = $('.conversion-unit');
+if (key.toLowerCase() === 'temperature'){//
+    allInputField.bind('input',function () {
+        let userInput = $(this);
+        if(userInput.attr('id') === 'celsius'){
+            $("#fahrenheit").val((Number(userInput.val())*1.8 + 32).toPrecision(12));
+            $("#kelvin").val((Number(userInput.val()) + 273.15).toPrecision(12))
+        }
+        if (userInput.attr('id') === 'fahrenheit'){
+            $('#celsius').val(((Number(userInput.val())-32) / 1.8).toPrecision(12));
+            $("#kelvin").val((((Number(userInput.val())-32) / 1.8) + 273.15).toPrecision(12));
+        }
+        if (userInput.attr('id') === 'kelvin'){
+            $('#celsius').val((Number(userInput.val())-273.15).toPrecision(12));
+            $("#fahrenheit").val(((Number(userInput.val())-273.15)*1.8 + 32).toPrecision(12));
+        }
+        if (userInput.val() === ''){
+            for (let inputField of allInputField){
+                if(inputField !== userInput){
+                    $(inputField).val('')
+                }
+            }
+        }
+    })
+}
+else {
+    $.ajax({
+        url: '/api/getRate',
+        method: 'post',
+        data: {
+            key: key
+        },
+        success: function (result){
+            calculationUsingRateFromServer(result);
+        }
+    })
+}
+
 
 function calculationUsingRateFromServer(result) {
     conversionRateFromDatabase = result;
-    console.log(result);
 }
 
-let allInputFlied = $('.conversion-unit');
-allInputFlied.bind('input',async function() {
+allInputField.bind('input',async function() {
     let UserInput = $(this);
     let userInputRate = {};
     await conversionRateFromDatabase.forEach(e => {
         if (e.DonViQuyDoi === Number(UserInput.attr('name'))){
             userInputRate = e;
-            console.log(userInputRate);
         }
     });
     for (let conversionRate of conversionRateFromDatabase){
-        for(let inputField of allInputFlied) {
+        for(let inputField of allInputField) {
             if (Number($(inputField).attr('name')) !== userInputRate.DonViQuyDoi){
                 if (Number($(inputField).attr('name')) === conversionRate.DonViQuyDoi){
                     let rateOfThisElement = Number(conversionRate.TiLe);
                     let resultOfConversion = (Number(UserInput.val())*Number(userInputRate.TiLe)) / rateOfThisElement;
-                    console.log(resultOfConversion);
                     if (UserInput.val() === '' || isNaN(UserInput.val()) || resultOfConversion == 'Infinity'){
                         $(inputField).val('');
                     }else {
@@ -44,16 +68,3 @@ allInputFlied.bind('input',async function() {
         }
     }
 })
-
-// allInputFlied.bind('focusin',function () {
-//     $(this).css({
-//         'background-color' : 'rgb(248,248,248)',
-//         'border': '1px solid #ced4da'});
-// })
-// allInputFlied.bind('focusout',function () {
-//     $(this).css({
-//         'background-color' : 'white',
-//         'border':'1px solid #ced4da'
-//     });
-// })
-
